79c0a10b7345f0f12a201563bd7d876046b520e6
maertin <mutuverria00gmail.com>
2025-07-11 19:14:19 -0300

    soluciona errores de deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
-
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
@@ -15,53 +14,84 @@ import { AuthGuard } from './guards/auth.guard';
 //Podemos cargar todas las rutas en este solo archivo o hacer un routing
 // para cada componente
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent,
-        data: {
-    renderMode: 'no-preference'
-  }
-     },
-    { path: 'signup', component: SignupComponent,
-      data: {
-    renderMode: 'no-preference'
-  }
-     },
-    { path: 'productos', component: ProductosComponent,data: {
-    renderMode: 'no-preference'
-  } },
-    { path: 'home', component: HomeComponent,
-      data: {
-    renderMode: 'no-preference'
-  }
-     },
-    { path: 'ver-detalle-producto/:id', component: VerDetalleProductoComponent,
-      data: {
-    renderMode: 'no-preference'
-  }
-     },
-    { path: 'carrito', component: CarritoComponent,
-      data: {
-    renderMode: 'no-preference'
-  }
-     },
-    { path: 'recuperar', component: RecuperarContrasenaComponent,data: {
-    renderMode: 'no-preference'
-  } },
-    { path: 'admin/productos', component: AdminProductosComponent, canActivate: [AuthGuard],
-      data: {
-    renderMode: 'no-preference'
-  }
-     },
-    { path: 'admin/productos/crear', component: AdminProductoCrearComponent, canActivate: [AuthGuard],
-      data: {
-    renderMode: 'no-preference'
-  }
-     },
-    { path: 'admin/productos/editar/:id', component: AdminProductoEditarComponent, canActivate: [AuthGuard] , data: {
-    renderMode: 'no-preference'
-  }},
-    { path: '**', redirectTo: 'home' ,
-      data: {
-    renderMode: 'no-preference'
-  }
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      renderMode: 'no-preference',
     },
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'productos',
+    component: ProductosComponent,
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'ver-detalle-producto/:id',
+    component: VerDetalleProductoComponent,
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'carrito',
+    component: CarritoComponent,
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'recuperar',
+    component: RecuperarContrasenaComponent,
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'admin/productos',
+    component: AdminProductosComponent,
+    canActivate: [AuthGuard],
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'admin/productos/crear',
+    component: AdminProductoCrearComponent,
+    canActivate: [AuthGuard],
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: 'admin/productos/editar/:id',
+    component: AdminProductoEditarComponent,
+    canActivate: [AuthGuard],
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    data: {
+      renderMode: 'no-preference',
+    },
+  },
 ];
