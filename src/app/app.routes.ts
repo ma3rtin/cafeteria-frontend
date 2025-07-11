a86739ffb8b6cc6e53953c835e55715c4ab92165
maertin <mutuverria00gmail.com>
2025-07-11 18:15:20 -0300

    arregla rutas para el deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,15 +15,53 @@ import { AuthGuard } from './guards/auth.guard';
 //Podemos cargar todas las rutas en este solo archivo o hacer un routing
 // para cada componente
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: 'productos', component: ProductosComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'ver-detalle-producto/:id', component: VerDetalleProductoComponent },
-    { path: 'carrito', component: CarritoComponent },
-    { path: 'recuperar', component: RecuperarContrasenaComponent },
-    { path: 'admin/productos', component: AdminProductosComponent, canActivate: [AuthGuard] },
-    { path: 'admin/productos/crear', component: AdminProductoCrearComponent, canActivate: [AuthGuard] },
-    { path: 'admin/productos/editar/:id', component: AdminProductoEditarComponent, canActivate: [AuthGuard] },
-    { path: '**', redirectTo: 'home' },
+    { path: 'login', component: LoginComponent,
+        data: {
+    renderMode: 'no-preference'
+  }
+     },
+    { path: 'signup', component: SignupComponent,
+      data: {
+    renderMode: 'no-preference'
+  }
+     },
+    { path: 'productos', component: ProductosComponent,data: {
+    renderMode: 'no-preference'
+  } },
+    { path: 'home', component: HomeComponent,
+      data: {
+    renderMode: 'no-preference'
+  }
+     },
+    { path: 'ver-detalle-producto/:id', component: VerDetalleProductoComponent,
+      data: {
+    renderMode: 'no-preference'
+  }
+     },
+    { path: 'carrito', component: CarritoComponent,
+      data: {
+    renderMode: 'no-preference'
+  }
+     },
+    { path: 'recuperar', component: RecuperarContrasenaComponent,data: {
+    renderMode: 'no-preference'
+  } },
+    { path: 'admin/productos', component: AdminProductosComponent, canActivate: [AuthGuard],
+      data: {
+    renderMode: 'no-preference'
+  }
+     },
+    { path: 'admin/productos/crear', component: AdminProductoCrearComponent, canActivate: [AuthGuard],
+      data: {
+    renderMode: 'no-preference'
+  }
+     },
+    { path: 'admin/productos/editar/:id', component: AdminProductoEditarComponent, canActivate: [AuthGuard] , data: {
+    renderMode: 'no-preference'
+  }},
+    { path: '**', redirectTo: 'home' ,
+      data: {
+    renderMode: 'no-preference'
+  }
+    },
 ];
